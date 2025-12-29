f86b50195d4317104f5ce4fe5c5378b562603bd6
matildasoderhall <soderhall.matilda@gmail.com>
2025-12-29 14:19:45 +0100

    🔧 change to erasableSyntaxOnly false, add aliases for paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
 })
