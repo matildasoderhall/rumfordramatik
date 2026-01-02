ed0b1efac94d561ca8f689849007c48824f4e7b5
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-02 09:53:51 +0100

    ✨ implement components Icon and MainNav in header and style according to design

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,3 +1,25 @@
+import { Icon } from '@/components/Icon';
+import { NavLink } from 'react-router';
+import { IconFill, IconSize } from '@components/Icon/Icon.types';
+import { LogoWithText } from '@/components/CustomIcons';
+import classNames from 'classnames';
+import styles from './Header.module.scss';
+import { MainNav } from './Nav';
+
 export const Header = () => {
-  return <></>;
+  return (
+    <header className={classNames(styles.header)}>
+      <div className={styles.innerWrapper}>
+        <NavLink to="/">
+          <Icon
+            icon={LogoWithText}
+            fill={IconFill.OnDark}
+            size={IconSize.Space2XL}
+          ></Icon>
+        </NavLink>
+        <h2 className={styles.headerText}>Rum för dramatik</h2>
+        <MainNav />
+      </div>
+    </header>
+  );
 };
