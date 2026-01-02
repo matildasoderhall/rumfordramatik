1d48c9e01aa88fa45a456c99a7b1b40c25079d72
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-02 09:53:10 +0100

    ✨ add main nav with styling for header

---
+++ src/layouts/Header/Nav/MainNav.tsx
@@ -0,0 +1,65 @@
+import { XIcon, ListIcon } from '@phosphor-icons/react';
+import styles from './MainNav.module.scss';
+import classNames from 'classnames';
+import { useState } from 'react';
+import { NavLink } from 'react-router';
+
+const NAV_ITEMS = [
+  { label: 'Hem', path: '/' },
+  { label: 'Prenumerera', path: 'sign-up' },
+  { label: 'Om oss', path: 'about-us' },
+  { label: 'Open Call', path: 'open-call' },
+  { label: 'Arkiv', path: 'archive' },
+];
+
+export const MainNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        className={classNames(styles.menuToggleBtn, { [styles.hidden]: isOpen })}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Open menu"
+        aria-expanded={isOpen}
+      >
+        <ListIcon size={32} aria-hidden={true} />
+      </button>
+
+      <nav
+        className={classNames(styles.nav, { [styles.isOpen]: isOpen })}
+        aria-label="Main"
+      >
+        <button
+          className={styles.closeBtn}
+          onClick={() => setIsOpen(false)}
+          aria-label="Close menu"
+        >
+          <XIcon size={32} aria-hidden={true} />
+        </button>
+        <ul className={classNames(styles.navList)}>
+          {NAV_ITEMS.map((item) => (
+            <li key={item.label}>
+              <NavLink
+                to={item.path}
+                onClick={() => setIsOpen(!isOpen)}
+                className={({ isActive }) =>
+                  classNames(styles.link, { [styles.active]: isActive })
+                }
+              >
+                {({ isActive }) => (
+                  <span aria-current={isActive ? 'page' : undefined}>
+                    {item.label}
+                  </span>
+                )}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className={classNames(styles.backdrop, { [styles.isOpen]: isOpen})}
+      onClick={() => setIsOpen(false)}
+      />
+    </>
+  );
+};
