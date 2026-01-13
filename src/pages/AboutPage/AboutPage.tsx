5d900d11d073261981c2952e7d610fbc2b7df105
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-13 11:28:12 +0100

    ✨ add initial tsx structure

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,5 +2,45 @@ import styles from './AboutPage.module.scss';
 import classNames from 'classnames';
 
 export const AboutPage = () => {
-  return <div className={classNames(styles.aboutPage)}></div>;
+  return (
+    <div className={classNames(styles.aboutPage)}>
+      <h1 className={styles.title}>Om oss</h1>
+
+      <div className={styles.imgWrapperTop}>
+        <img
+          src="http://rumfordramatik.se/admin/wp-content/uploads/2026/01/test.jpeg"
+          alt=""
+        />
+      </div>
+      <span className={styles.groupchatSticker}>
+        Det började i en gruppchatt
+      </span>
+      <div className={styles.imgWrapperBottom}>
+        <img
+          src="http://rumfordramatik.se/admin/wp-content/uploads/2026/01/test.jpeg"
+          alt=""
+        />
+      </div>
+
+      <div className={styles.aboutTextWrapper}>
+        <p>
+          Vi är en fysisk tidskrift som publicerar nyskriven dramatik på
+          svenska, men även på norska och danska tillsammans med svensk
+          översättning. Tidskriften vänder sig till dem som har ett intresse för
+          dramatik, text och scenkonst.
+        </p>
+        <p>
+          Med tidskriften vill vi skapa en mötesplats för den samtida
+          dramatiken, utforska vad dramatisk text kan vara idag och ge en
+          alternativ möjlighet för dramatiker att bli publicerade.
+        </p>
+      </div>
+      <span className={styles.meetingspotSticker}>mötesplats</span>
+      <p className={styles.contactText}>
+        Har du några funderingar eller vill komma i kontakt med oss kan du
+        skriva till oss på sociala medier eller mejla till
+        <a href="mailto:info@rumfordramatik.se"> info@rumfordramatik.se</a>.
+      </p>
+    </div>
+  );
 };
