65f32d393f9ccefa45eb2d6cb4b6d95e619c3542
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-16 11:16:08 +0100

    ✨ add props and styling for scribble circle

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,24 @@
 import styles from './Button.module.scss';
 import classNames from 'classnames';
+import { ButtonType } from './Button.types';
+
 
 interface ButtonProps {
-  children: string;
-  disabled?: boolean;
-  className?: string;
+ children: string;
+ type: ButtonType;
+ disabled?: boolean;
+ loading?: boolean;
+ circled?: boolean;
+ className?: string;
 }
 
-export const Button = ({ children, disabled, className }: ButtonProps) => {
-  return (
-    <button className={classNames(
-      styles.button, className
-    )}>
-      <span className={styles.buttonText}>{children}</span>
-    </button>
-  );
+
+export const Button = ({ children, type = ButtonType.Button, disabled, loading, circled = false,  className }: ButtonProps) => {
+ return (
+   <button type={type} disabled={disabled || loading} className={classNames(
+     styles.button, { [styles.circled]: circled}, className
+   )}>
+     <span className={styles.buttonText}>{children}</span>
+   </button>
+ );
 };
