8b475484c9f71c9f5a13b494976f59caf19ced7d
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-26 10:42:16 +0100

    ✨ add more props and format code

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,23 +2,37 @@ import styles from './Button.module.scss';
 import classNames from 'classnames';
 import { ButtonType } from './Button.types';
 
-
 interface ButtonProps {
- children: string;
- type: ButtonType;
- disabled?: boolean;
- loading?: boolean;
- circled?: boolean;
- className?: string;
+  children: string;
+  type: ButtonType;
+  disabled?: boolean;
+  loading?: boolean;
+  onClick?: () => void;
+  circled?: boolean;
+  className?: string;
 }
 
-
-export const Button = ({ children, type = ButtonType.Button, disabled, loading, circled = false,  className }: ButtonProps) => {
- return (
-   <button type={type} disabled={disabled || loading} className={classNames(
-     styles.button, { [styles.circled]: circled}, className
-   )}>
-     <span className={styles.buttonText}>{children}</span>
-   </button>
- );
+export const Button = ({
+  children,
+  type = ButtonType.Button,
+  disabled,
+  loading,
+  onClick,
+  circled = false,
+  className,
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      disabled={disabled || loading}
+      onClick={onClick}
+      className={classNames(
+        styles.button,
+        { [styles.circled]: circled },
+        className
+      )}
+    >
+      <span className={styles.buttonText}>{children}</span>
+    </button>
+  );
 };
