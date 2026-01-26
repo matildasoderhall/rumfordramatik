b08ecf3716c7edab6b8929e55dc59f6815c90c8e
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-26 15:46:48 +0100

    ✨ add styling and content to SignUpPage

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -5,7 +5,17 @@ import classNames from 'classnames';
 export const SignUpPage = () => {
   return (
     <div className={classNames(styles.signUpPage)}>
-      <NewsletterContainer />
+      <h1>Nyhetsbrev</h1>
+      <div className={styles.mainContentWrapper}>
+        <div className={styles.infoTextWrapper}>
+          <p className={styles.infoText}>
+            Nedan kan du signa upp för att prenumerera på vårt nyhetsbrev.
+          </p>
+          <span className={styles.stickerText}>Missa inget!</span>
+        </div>
+
+        <NewsletterContainer />
+      </div>
     </div>
   );
 };
