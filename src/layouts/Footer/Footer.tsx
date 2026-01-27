507a4c4758eaaa6e7ee6d54fdd5995cbbee8369f
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-27 16:47:25 +0100

    ✨ add link to PrivacyPolicy in footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,18 +1,20 @@
+import { Link } from 'react-router';
 import styles from './Footer.module.scss';
 import { SocialsNav } from './SocialsNav';
+import classNames from 'classnames';
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.links}>
+      <div className={classNames(styles.socialMediaLinks, styles.link)}>
         <a href="mailto:info@rumfordramatik.se" className={styles.link}>
           info@rumfordramatik.se
         </a>
         <SocialsNav />
       </div>
-      <p>©{currentYear} Rum för dramatik.</p>
+      <p>©{currentYear} Rum för dramatik |  <Link className={styles.link} to={"/integritetspolicy"}>Integritetspolicy</Link></p>
     </footer>
   );
 };
