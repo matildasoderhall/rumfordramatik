c688340600e8f7833fe8fc1e046853278ef131d8
matildasoderhall <soderhall.matilda@gmail.com>
2026-01-28 16:59:25 +0100

    ⚡️ add loading lazy

--- src/components/CommingEvents/CommingEvents.tsx
+++ src/components/CommingEvents/CommingEvents.tsx
@@ -104,6 +104,7 @@ export const CommingEvents = ({
                     height={image.height}
                     width={image.width}
                     className={styles.img}
+                    loading='lazy'
                   />
                 )}
               </div>
